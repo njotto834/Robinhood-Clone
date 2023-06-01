3880badd6cc6946662a0982c2eb0ea6dfe6e314c
Nicholas Otto <nick83487@gmail.com>
2023-06-01 15:53:27 -0500

    Got a working tooltip and a method to temporarily create mock data. Also set the x axis to a time scale and formatted the tooltip accordingly.

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import {
     Chart as ChartJS,
     CategoryScale,
     LinearScale,
+    TimeScale,
     PointElement,
-    LineElement,
+    LineElement, 
+    Tooltip,
   } from 'chart.js';
 import {Line} from "react-chartjs-2"
+import { format } from 'date-fns';
+import 'chartjs-adapter-date-fns';
+import {enUS} from 'date-fns/locale';
 
 function LineGraph() {
     ChartJS.register(
         CategoryScale,
         LinearScale,
+        TimeScale,
         PointElement,
         LineElement,
+        Tooltip,
     );
 
+    const [ graphData, setGraphData ] = useState([]);
+
     const data = {
-        labels: ['Mon', 'Tue', 'Wed'],
         datasets: [{
-            data: [10, 50, 100],
+            data: graphData,
             backgroundColor: "black",
             borderColor: "#5AC53B",
             borderWidth: 2,
@@ -36,15 +44,51 @@ function LineGraph() {
         plugins: {
             legend: true
         },
+        tooltips: {
+            enabled: true,
+            mode: 'index',
+            intersect: false,
+        },
         scales: {
             x: {
-                display: false // Hide x-axis labels
+                type: 'time',
+                time: {
+                    format: "MM/DD/YY",
+                    unit: "day",
+                    tooltipFormat: "MMM dd, yyyy"
+                },
+                display: false, // Hide x-axis labels
+                adapters: {
+                    date: {
+                        locale: enUS
+                    }
+                }
             },
             y: {
+                type: 'linear',
                 display: false // Hide y-axis labels
-            }
+                
+            },
+            
         }
     }
+
+    const createMockData = () => {
+        let data = [];
+        let value = 50;
+        for (var i = 0; i < 366; i++){
+            let date = new Date();
+            date.setHours(0, 0, 0, 0);
+            date.setDate(i);
+            value += Math.floor(Math.random() * 21) - 10;
+            data.push({x: date, y: value});
+        }
+        setGraphData(data);
+    }
+
+    useEffect(() => { //runs once on the loading of the component?
+        createMockData();
+    }, [])
 
     return (
     <div className="linegraph">
