9e14b569acd081cb0eeaaa337a11cf6e5962c1cb
Nicholas Otto <nick83487@gmail.com>
2023-07-06 17:47:21 -0500

    Added a buying power section, market news section, and started on popular lists

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Newsfeed.css'
 import LineGraph from './LineGraph.js'
 import TimeLine from './TimeLine.js'
+import Chip from './Chip'
 
 function Newsfeed() {
+  const [popularTopics, setTopics] = useState([
+    "Technology",
+    "Top Movers",
+    "Upcoming Earnings",
+    "Crypto",
+    "Cannabis",
+    "Healthcare Supplies",
+    "Index ETFs",
+    "Technology",
+    "China",
+    "Pharma",
+  ]);
+
   return (
     <div className="newsfeed">
         <div className="newsfeed__container">
@@ -17,9 +31,29 @@ function Newsfeed() {
               <TimeLine />
             </div>
           </div>
-          <div className="newfeed__buying__section">
+          <div className="newsfeed__buying__section">
             <h2> Buying Power</h2>
             <h2> $100.00</h2>
+          </div>
+          <div className="newsfeed__market__section">
+            <div className="newsfeed__market__box">
+              <p> Markets Closed</p>
+              <h1> Happy Thanksgiving</h1>
+            </div>
+          </div>
+          <div className="newsfeed__popularlists__section">
+            <div className="newsfeed__popularlists__intro">
+              <h1>Popular Lists</h1>
+              <p>Show More</p>
+            </div>
+            <div className="newsfeed__popularlists__badges">
+              {popularTopics.map((topic) => (
+                <Chip 
+                  label={topic}
+                  image={`https://avatars.dicebear.com/api/human/${topic}.svg`}
+                />
+              ))}
+            </div>
           </div>
         </div>
     </div>
